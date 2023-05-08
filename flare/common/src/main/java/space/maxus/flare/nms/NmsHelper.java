d3cfaf13ee6c2db69b393986bab2dcad9c7e0823
maxus <maxus.info.mail@gmail.com>
2023-05-08 17:30:59 +0300

    Common logic docs

--- flare/common/src/main/java/space/maxus/flare/nms/NmsHelper.java
+++ flare/common/src/main/java/space/maxus/flare/nms/NmsHelper.java
@@ -8,7 +8,14 @@ import space.maxus.flare.nms.generic.ReflectionHelper;
 
 import java.lang.reflect.InvocationTargetException;
 
+/**
+ * A utility interface that does version-dependent logic
+ */
 public interface NmsHelper {
+    /**
+     * Creates a new NMS helper based on the current version
+     * @return {@link ReflectingNmsHelper} if version is unknown, or a version-dependent implementation otherwise
+     */
     static @NotNull NmsHelper getInstance() {
         NmsVersion version = ReflectionHelper.NMS_VERSION;
         if (version == NmsVersion.UNKNOWN || version == NmsVersion.UNPREFIXED) // currently don't have an implementation for unprefixed NMS
@@ -24,11 +31,32 @@ public interface NmsHelper {
         return new ReflectingNmsHelper();
     }
 
+    /**
+     * Gets version this NMS helper supports
+     * @return NMS version
+     */
     NmsVersion getVersion();
 
+    /**
+     * Obtains player connection ({@link net.minecraft.server.network.ServerGamePacketListenerImpl })
+     * @param player Player, whose connection to obtain
+     * @return connection for packet sending
+     */
     Object obtainConnection(Player player);
 
+    /**
+     * Builds an inventory title packet ({@link net.minecraft.network.protocol.game.ClientboundOpenScreenPacket})
+     * for this player.
+     * @param player player to be used
+     * @param newTitle title to be displayed
+     * @return OpenScreenPacket
+     */
     Object buildTitlePacket(Player player, Component newTitle);
 
+    /**
+     * Sends a packet to this player
+     * @param connection connection to be used
+     * @param packet packet to be sent
+     */
     void sendPacket(Object connection, Object packet);
 }
--- flare/common/src/main/java/space/maxus/flare/nms/NmsVersion.java
+++ flare/common/src/main/java/space/maxus/flare/nms/NmsVersion.java
@@ -1,9 +1,19 @@
 package space.maxus.flare.nms;
 
+
+/**
+ * Contains different NMS versions
+ */
 public enum NmsVersion {
     v1_19_R1,
     v1_19_R2,
     v1_19_R3, // current latest supported
+    /**
+     * Undetermined version, reflection adapter will be used instead
+     */
     UNKNOWN,
+    /**
+     * Undetermined version, but it does not contain version infix (e.g. 1_19_R3)
+     */
     UNPREFIXED,
 }
